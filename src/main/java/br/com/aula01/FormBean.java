f367d71730c5d7e12e35543e40b34685b34adf3b
Felipe Menezes <lipe.menezes@live.com>
2017-09-11 22:22:04 -0300

    Added List 1 and 2

---
+++ src/main/java/br/com/aula01/FormBean.java
@@ -0,0 +1,116 @@
+package br.com.aula01;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean(name = "formulario")
+@SessionScoped
+
+public class FormBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String message;
+
+	private String nome;
+
+	private String matricula;
+
+	private char sexo;
+
+	private Double nota1;
+
+	private Double nota2;
+
+	private Double nota3;
+
+	private Double media;
+
+	private List<Double> listaDasMedias;
+
+	public void calcularMedia() throws IOException {
+
+		media = (nota1 + nota2 + nota3) / 3;
+
+		FacesContext.getCurrentInstance().getExternalContext().redirect("tabela.xhtml");
+
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public char getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+
+	public Double getNota1() {
+		return nota1;
+	}
+
+	public void setNota1(Double nota1) {
+		this.nota1 = nota1;
+	}
+
+	public Double getNota2() {
+		return nota2;
+	}
+
+	public void setNota2(Double nota2) {
+		this.nota2 = nota2;
+	}
+
+	public Double getNota3() {
+		return nota3;
+	}
+
+	public void setNota3(Double nota3) {
+		this.nota3 = nota3;
+	}
+
+	public Double getMedia() {
+		return media;
+	}
+
+	public void setMedia(Double media) {
+		this.media = media;
+	}
+
+	public List<Double> getListaDasMedias() {
+		return listaDasMedias;
+	}
+
+	public void setListaDasMedias(List<Double> listaDasMedias) {
+		this.listaDasMedias = listaDasMedias;
+	}
+
+}
---
+++ src/main/java/dao/ConnectionFactory.java
@@ -0,0 +1,47 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Classe respons·vel por criar e destruir conex√µes com bancos de dados
+ * 
+ * O banco escolhido foi o MySQL, assim È necess·rio utilizar o respectivo
+ * driver JDBC
+ * 
+ * @author Vilmar C. Pereira J√∫nior
+ * 
+ *         Disciplina de Desenvolvimento Web Senac 2017.1
+ *
+ */
+public class ConnectionFactory {
+
+	public Connection obterConexao() {
+		String nomeEsquema = "dbprojeto1";
+		String enderecoBanco = "jdbc:mysql://localhost/" + nomeEsquema;
+		String usuario = "root";
+		String senha = "1234";
+		String driverJDBC = "com.mysql.jdbc.Driver";
+
+		try {
+			Class.forName(driverJDBC);
+			Connection conexao = DriverManager.getConnection(enderecoBanco, usuario, senha);
+			System.out.println("Conex„o aberta");
+			return conexao;
+		} catch (SQLException | ClassNotFoundException e) {
+			System.out.println("Erro ao obter conex„o com o banco: " + e.getMessage());
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static void fecharConexao(Connection con) {
+		try {
+			con.close();
+			System.out.println("Conex„o fechada");
+		} catch (SQLException e) {
+			System.out.println(e);
+			throw new RuntimeException(e);
+		}
+	}
+}
